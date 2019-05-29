f33296c7bb739badd2c503918c1cd0e6d7df9bf0
yann <tmp4321@qq.com>
2019-05-29 18:17:40 +0800

    base zookeeper client

---
+++ 09-21  Alibaba Nacos Discovery/zookeeper-client-demo/src/main/java/demo/zookeeperclientdemo/ZookeeperClientDemoApplication.java
@@ -0,0 +1,15 @@
+package demo.zookeeperclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @EnableDiscoveryClient
+
+public class ZookeeperClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookeeperClientDemoApplication.class, args);
+	}
+
+}
