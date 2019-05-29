df23236dca0af19e18855a1551a44cebd58faa94
yann <tmp4321@qq.com>
2019-05-29 16:40:19 +0800

    base Eureka client

---
+++ 09-21  Alibaba Nacos Discovery/eureka-client-demo/src/main/java/demo/eurekaclientdemo/EurekaClientDemoApplication.java
@@ -0,0 +1,15 @@
+package demo.eurekaclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @EnableDiscoveryClient
+
+public class EurekaClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientDemoApplication.class, args);
+	}
+
+}
