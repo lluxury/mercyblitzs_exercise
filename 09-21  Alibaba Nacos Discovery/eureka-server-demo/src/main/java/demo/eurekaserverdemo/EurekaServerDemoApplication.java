522b62e00cda409cd820c19a8b584f1b0ae49e98
yann <tmp4321@qq.com>
2019-05-30 15:42:06 +0800

    fix conf

--- 09-21  Alibaba Nacos Discovery/eureka-server-demo/src/main/java/demo/eurekaserverdemo/EurekaServerDemoApplication.java
+++ 09-21  Alibaba Nacos Discovery/eureka-server-demo/src/main/java/demo/eurekaserverdemo/EurekaServerDemoApplication.java
@@ -2,9 +2,11 @@ package demo.eurekaserverdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplicationl
-// @EnableEurekaServer
+@SpringBootApplication
+@EnableEurekaServer
+
 public class EurekaServerDemoApplication {
 
 	public static void main(String[] args) {
--- 09-21  Alibaba Nacos Discovery/zookeeper-client-demo/src/main/java/demo/zookeeperclientdemo/ZookeeperClientDemoApplication.java
+++ 09-21  Alibaba Nacos Discovery/zookeeper-client-demo/src/main/java/demo/zookeeperclientdemo/ZookeeperClientDemoApplication.java
@@ -2,9 +2,10 @@ package demo.zookeeperclientdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 
 public class ZookeeperClientDemoApplication {
 
